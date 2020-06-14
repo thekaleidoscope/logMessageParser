9e24f93c212c0bf150e8515e4ff5cfb447d5d3a6
Yadhukrishna S Pai <yadhukrishnap@sahajsoft.com>
2020-06-14 14:16:55 +0530

    add error log message parser

---
+++ main/LogMessage/ErrorLogMessage.py
@@ -0,0 +1,29 @@
+import re
+
+from main.LogMessage.LogMessage import LogMessage
+from main.MessageType import MessageType
+from main.Unknown import Unknown
+
+
+class ErrorLogMessage(LogMessage):
+    def __init__(self, messageType, logLevel, timestamp, message) -> None:
+        self.messageType = messageType
+        self.logLevel = logLevel
+        self.timestamp = timestamp
+        self.message = message
+
+    def __eq__(self, other) -> bool:
+        return self.__class__ == other.__class__ and \
+               self.message == other.message and \
+               self.timestamp == other.timestamp and \
+               self.logLevel == other.logLevel and \
+               self.messageType == other.messageType
+
+    @classmethod
+    def parse(cls, input_log_message):
+        expected_pattern = 'E\s(\d*)\s(\d*)\s(.*)'
+        match = re.search(expected_pattern, input_log_message)
+        if match:
+            return ErrorLogMessage(MessageType.ERROR, int(match.group(1)), int(match.group(2)), match.group(3))
+        else:
+            return Unknown(input_log_message)
--- test/logMessageParsingTest.py
+++ test/logMessageParsingTest.py
@@ -1,5 +1,6 @@
 import unittest
 
+from main.LogMessage.ErrorLogMessage import ErrorLogMessage
 from main.LogMessage.InformationLogMessage import InformationLogMessage
 from main.LogMessage.WarningLogMessage import WarningLogMessage
 from main.MessageType import MessageType
@@ -34,4 +35,18 @@ class LogMessageParsingTest(unittest.TestCase):
         expected_log_message = Unknown(input_log_message)
 
         log_message_parsed = WarningLogMessage.parse(input_log_message)
+        self.assertEqual(expected_log_message, log_message_parsed)
+
+    def test_shouldTakeInputErrorLog(self):
+        input_log_message = "E 2 6 Completed armadillo processing"
+        expected_log_message = ErrorLogMessage(MessageType.ERROR, 2, 6, "Completed armadillo processing")
+
+        log_message_parsed = ErrorLogMessage.parse(input_log_message)
+        self.assertEqual(expected_log_message, log_message_parsed)
+
+    def test_shouldReturnUnknownWhenInputIncorrectlyFormattedErrorLogMessage(self):
+        input_log_message = "Completed armadillo processing"
+        expected_log_message = Unknown(input_log_message)
+
+        log_message_parsed = ErrorLogMessage.parse(input_log_message)
         self.assertEqual(expected_log_message, log_message_parsed)
